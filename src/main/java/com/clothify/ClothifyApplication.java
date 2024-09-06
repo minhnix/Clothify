b13401c1c8b479b0666745da587eb0a54f441f4b
Minh <bhminh322@gmail.com>
2024-09-06 13:46:59 +0700

    init project

---
+++ src/main/java/com/clothify/ClothifyApplication.java
@@ -0,0 +1,13 @@
+package com.clothify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clothify/ClothifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clothify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
